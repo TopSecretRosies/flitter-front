aae582e04d66a9f17098e71f427360ad1b2b347f
Estela.Fernandez <estelafdez94@gmail.com>
2023-02-11 23:52:48 +0100

    5

--- src/components/PubliCation.vue
+++ src/components/PubliCation.vue
@@ -1,32 +1,33 @@
 <template>
     <div class="post container">
         <div class="row">
-            <div class="user-avatar col-5" style="background-color: green;">
+            <div class="user-avatar col-6 " style="background-color: green; ">
                 <img src="../assets/mujer.jpg" />
             </div>
-            <div class="post-content col-1">
+            <div class="post-content col-6">
                 <div class="post-user-info" style="background-color: blue;">
                     <h4>{{ publication.author }}</h4>
                     <i class="fas fa-check-circle"></i>
                     <!-- <p class="publication-date"> {{ publication.publicationDate }}</p> -->
                 </div>
             </div>
-            <p class="post-text  col-4" style="background-color: yellow"> {{ publication.text }}</p>
+            <p class="post-text row col-12" style="background-color: yellow;"> {{ publication.text }}</p>
             <div class="post-image">
                 <!-- <img :src="publication.image"/> -->
                 <img class="img-fluid" :src="publication.image" style="width: 40%;" />
             </div>
+            <div class="post-icons col-12 " style="background-color: violet;">
+                <i class="far fa-comment"></i>
+                <i class="fas fa-retweet"></i>
+                <i class="far fa-heart"></i>
+                <!-- <p>{{ publication.kudos }} kudos</p> -->
+                <i class="fas fa-share-alt"></i>
+
+            </div>
         </div>
     </div>
 
-    <div class="post-icons col-12 " style="background-color: violet;">
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <!-- <p>{{ publication.kudos }} kudos</p> -->
-        <i class="fas fa-share-alt"></i>
 
-    </div>
 </template>
 
 <script lang="ts">
@@ -45,76 +46,12 @@ export default defineComponent({
 
 <style scoped>
 * {
-    max-width: 100%;
-}
-
-.user-avatar {
-    width: 6rem;
-    height: 6rem;
-    flex-shrink: 0;
+max-width: 100%;
 }
 
 .user-avatar img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 50%;
-}
-
-.post-user-info {
-    display: flex;
-    margin: 1rem 14px;
-
-}
-
-.post-user-info h4 {
-    font-size: 1.6rem;
-    align-items: center;
-    margin-right: 0.5rem;
-}
-
-.post-user-info i {
-    font-size: 1.6rem;
-    color: black;
-    margin-right: 0.5rem;
-}
-
-.post-text {
-    font-size: 1.6rem;
-    margin-bottom: 1rem;
-}
-
-#post {
-    display: flex;
-    padding: 1.5rem;
-    border-bottom: 0.1 rem solid #eee;
-}
-
-.post-img {
-    width: 48rem;
-    height: 30rem;
-}
-
-
-
-.post-icons {
-    margin-top: 1rem;
-}
-
-.post-icons i {
-    font-size: 1.8rem;
-    color: #868383;
-    margin-right: 6rem;
-}
-
-@media screen and (max-width:480px) {
-    .post {
-        display: flex;
-        flex-direction: column;
-        padding-left: 6rem;
-        border-bottom: 0.1 rem solid #eee;
-    }
-
-
+max-width: 40% ;
+object-fit: cover;
+border-radius: 50%;
 }
 </style>
