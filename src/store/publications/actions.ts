2a27c5a5d2b11e03b360dcc69f862e86d38ab865
Carol <contact@mail.com>
2023-02-14 21:11:50 +0100

    sunir rama

--- src/store/publications/actions.ts
+++ src/store/publications/actions.ts
@@ -9,7 +9,7 @@ const actions: ActionTree<IPublicationState, IState> = {
     async fetchPublications({commit}) {
         commit("setIsLoading", true);
         const{ data } = await flitterApi.get<unknown, AxiosResponse<Publications[]>>(
-            '/posts?skip=0&limit=5'
+            '/posts'
         );
         commit('setIsLoading', false);
         commit('setPublications', data);
